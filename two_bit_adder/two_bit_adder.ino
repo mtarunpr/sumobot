c9dcd05a31be144299066b73eba7ddb58c157312
mtarunpr <m.tarunprasad@gmail.com>
2019-10-12 16:20:29 -0400

    Two bit adder - working version

--- two_bit_adder/two_bit_adder.ino
+++ two_bit_adder/two_bit_adder.ino
@@ -3,6 +3,8 @@ const int LED_PIN2 = 10;
 const int BUTTON_PIN1 = 14;
 const int BUTTON_PIN2 = 15;
 
+int state;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN1, OUTPUT);
@@ -12,13 +14,60 @@ void setup() {
 
   digitalWrite(LED_PIN1, LOW);
   digitalWrite(LED_PIN2, LOW);
+
+  state = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(BUTTON_PIN2) == HIGH)
   {
-    digitalWrite(LED_PIN1, LOW);
-    digitalWrite(LED_PIN2, HIGH);
+    switch (state)
+    {
+      case 0:
+        digitalWrite(LED_PIN1, LOW);
+        digitalWrite(LED_PIN2, HIGH);
+        state = 1;
+        break;
+
+      case 1:
+        digitalWrite(LED_PIN1, HIGH);
+        digitalWrite(LED_PIN2, LOW);
+        state = 2;
+        break;
+
+      case 2:
+        digitalWrite(LED_PIN1, HIGH);
+        digitalWrite(LED_PIN2, HIGH);
+        state = 3;
+        break;
+    }
+    delay(200);
   }
+
+  if (digitalRead(BUTTON_PIN1) == HIGH)
+  {
+    switch (state)
+    {
+      case 1:
+        digitalWrite(LED_PIN1, LOW);
+        digitalWrite(LED_PIN2, LOW);
+        state = 0;
+        break;
+
+      case 2:
+        digitalWrite(LED_PIN1, LOW);
+        digitalWrite(LED_PIN2, HIGH);
+        state = 1;
+        break;
+
+      case 3:
+        digitalWrite(LED_PIN1, HIGH);
+        digitalWrite(LED_PIN2, LOW);
+        state = 2;
+        break;
+    }
+    delay(200);
+  }
+
 }
